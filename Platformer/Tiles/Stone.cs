d1bce1a5118ee0b4b911b6262f98ad2b01379a35
Cooper Saye <coopersaye@gmail.com>
2021-04-02 11:13:51 -0600

    Implement Tile class.

---
+++ Platformer/Tiles/Stone.cs
@@ -0,0 +1,12 @@
+﻿namespace Platformer.Tiles
+{
+    public class Stone : Tile
+    {
+        public Stone(int x, int y) : base(x, y) {}
+
+        public override void Draw(Game1 game)
+        {
+            Drawing.DrawSprite(Drawing.StoneTexture, Bounds, game, SortingLayers.Tiles);
+        }
+    }
+}
---
+++ Platformer/Tiles/Tile.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace Platformer.Tiles
+{
+    public abstract class Tile
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Rectangle Bounds
+        {
+            get { return new Rectangle(X, Y, Drawing.Grid, Drawing.Grid); }
+        }
+
+        public Tile(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public abstract void Draw(Game1 game);
+    }
+}
