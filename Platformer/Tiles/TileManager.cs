81781ced1799af336f51e3a195467c0ceaf5277c
Cooper Saye <coopersaye@gmail.com>
2021-04-02 11:56:36 -0600

    Implement collision detection.

--- Platformer/Tiles/TileManager.cs
+++ Platformer/Tiles/TileManager.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
 
 namespace Platformer.Tiles
 {
@@ -23,5 +25,43 @@ namespace Platformer.Tiles
         }
 
         public void Add(Tile tile) => Tiles.Add(tile);
+
+        public Vector2 TryMove(Player player, Vector2 movementFactor)
+        {
+            Rectangle newBounds = player.Bounds;
+            newBounds.X += (int)Math.Round(movementFactor.X);
+            newBounds.Y += (int)Math.Round(movementFactor.Y);
+
+            // Check each tile
+            foreach (Tile tile in Tiles)
+            {
+                // If new bounds intersects, adjust bounds
+                Rectangle tileBounds = tile.Bounds;
+                if (newBounds.Intersects(tileBounds))
+                {
+                    Point newCenter = newBounds.Center;
+                    Point objCenter = tileBounds.Center;
+
+                    // If greater horizontal displacement
+                    if (Math.Abs(newCenter.X - objCenter.X) > Math.Abs(newCenter.Y - objCenter.Y))
+                    {
+                        // If right of tile
+                        if (newCenter.X > objCenter.X) newBounds.X = tileBounds.Right;
+                        // If left of tile
+                        else newBounds.X = tileBounds.Left - newBounds.Width;
+                    }
+                    // If greater vertical displacement
+                    else
+                    {
+                        // If above tile
+                        if (newCenter.Y > objCenter.Y) newBounds.Y = tileBounds.Bottom;
+                        // If below tile
+                        else newBounds.Y = tileBounds.Top - newBounds.Height;
+                    }
+                }
+            }
+
+            return new Vector2(newBounds.X, newBounds.Y);
+        }
     }
 }
